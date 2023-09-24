20292beea341033e409adf92bd82841b59e0b6a0
minseo121 <kittse1217@gmail.com>
2023-09-25 01:30:20 +0900

    스크롤 및 1차 완성

--- User_page/search/search.js
+++ User_page/search/search.js
@@ -1,13 +1,26 @@
 const template = `
+    <div class="stamp1-set">
         <button class="stamp1">
-            <img class="empty-stamp" src="../../img/stamp1.png" alt="빈 스탬프 이미지">
+            <img src="../../img/stamp1.png" alt="빈 스탬프 이미지">
         </button>
         <button class="stamp2">
-            <img class="empty-stamp" src="../../img/stamp1.png" alt="빈 스탬프 이미지">
+            <img src="../../img/stamp1.png" alt="빈 스탬프 이미지">
         </button>
         <button class="stamp3">
-            <img class="empty-stamp" src="../../img/stamp1.png" alt="빈 스탬프 이미지">
+            <img src="../../img/stamp1.png" alt="빈 스탬프 이미지">
         </button>
+    </div>
+    <div class="stamp2-set">
+        <button class="stamp4">
+            <img src="../../img/stamp1.png" alt="빈 스탬프 이미지">
+        </button>
+        <button class="stamp5">
+            <img src="../../img/stamp1.png" alt="빈 스탬프 이미지">
+        </button>
+        <button class="stamp6">
+            <img src="../../img/stamp1.png" alt="빈 스탬프 이미지">
+        </button>
+    </div>
 `;
 $(".stampset").append(template);
 
@@ -17,9 +30,11 @@ const close=document.querySelector(".bi-x");
 
 function init(){
     open.addEventListener("click",function(){
+        document.querySelector(".modal-bg").classList.remove("hidden"); // 어두운 배경도 표시
         modal.classList.remove("hidden");
     });
     close.addEventListener("click",function(){
+        document.querySelector(".modal-bg").classList.add("hidden"); // 어두운 배경도 숨김
         modal.classList.add("hidden");
     });
 }
