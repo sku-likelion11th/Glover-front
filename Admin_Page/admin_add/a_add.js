b6d30a571d0c261d4f4191caa7b1af3944ca71c3
JunYoung02 <whwnsdud011@naver.com>
2023-09-24 15:51:44 +0900

    html 및 js 세팅

--- Admin_Page/admin_add/a_add.js
+++ Admin_Page/admin_add/a_add.js
@@ -8,3 +8,16 @@ function init() {
 }
 
 init();
+
+// form에서 이미지 업로드시 미리보기
+function readURL(input, previewId) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById(previewId).src = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        document.getElementById(previewId).src = "";
+    }
+}
