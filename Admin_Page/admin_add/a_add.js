41512b8cf1291a3450cbc4f3329d4c074d726126
JunYoung02 <whwnsdud011@naver.com>
2023-09-26 16:51:11 +0900

    깨진 부분 수정

--- Admin_Page/admin_add/a_add.js
+++ Admin_Page/admin_add/a_add.js
@@ -22,7 +22,7 @@ imgBoxes.forEach(function(imgBox) {
 });
 
 // 폼 내부 버튼에 따른 모달창 제공
-var form = document.getElementById("name");  // 폼 이름 (설명, 기간, 이미지 등)
+var submitButton = document.getElementById("submit");  // 폼 내부의 저장 버튼
 var deleteButton = document.querySelector('.del');  // 폼 내부의 삭제 버튼 
 var close = document.querySelector(".bi-x")          // 폼 내부의 'X' 버튼
 
@@ -32,22 +32,26 @@ var thiModal = document.getElementById("thi_modal");     // 세 번째 모달창
 
 
 // 저장 눌렀을 때 실행되며, 이에 맞는 모달이 나타남
-form.addEventListener("submit", function(event) {
-    event.preventDefault(); // 폼 제출 방지
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault(); // 버튼의 기본 동작 방지
 
     // 여기에서 POST 요청을 보내고 백엔드에서 응답을 받아 모달에 표시합니다.
+    
     // 모달 창 열기
-    firstModal.style.display = "block";
+    document.getElementById("fir_modal").style.display = "block";
 });
+
 
 // 삭제 눌렀을 때 실행되며, 이에 맞는 모달이 나타남
 function delModal() {
-    secModal.style.display = 'block';
+    document.getElementById("sec_modal").style.display = 'block';
 }
 deleteButton.addEventListener('click', delModal);
 
+
 // ('X') 눌렀을 때 실행되며, 이에 맞는 모달이 나타남
 function noneModal() {
-    thiModal.style.display = 'block';
+    document.getElementById("thi_modal").style.display = 'block';
 }
 close.addEventListener('click', noneModal);
+
